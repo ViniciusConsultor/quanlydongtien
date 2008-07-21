8f31195897d4f6d7721e6bfb73de4e3fa9bad735
thangh3t <thangh3t@1a0ceb71-3252-0410-a707-35779bdedb0a>
2008-07-21 01:58:46 +0000

    Update lai file xml config

--- trunk/Source/Quanlydongtien/Quanlydongtien/Program.cs
+++ trunk/Source/Quanlydongtien/Quanlydongtien/Program.cs
@@ -15,6 +15,6 @@ namespace Quanlydongtien
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainPoint());
-        hn}
+        }
     }
 }
