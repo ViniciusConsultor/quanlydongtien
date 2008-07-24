d87e9ad019dfdc6fd2abd619de7ecb36ea75f6db
thangh3t <thangh3t@1a0ceb71-3252-0410-a707-35779bdedb0a>
2008-07-24 05:08:37 +0000

    Nhap thong tin ve hop dong, add datepickuptime to datagridview

--- Source/Quanlydongtien/Quanlydongtien/NhapthongtinHD.Designer.cs
+++ Source/Quanlydongtien/Quanlydongtien/NhapthongtinHD.Designer.cs
@@ -31,30 +31,33 @@ namespace Quanlydongtien
             this.cbxMaKH = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtTongtien = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.cbxKyhan = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.cbxLaisuat = new System.Windows.Forms.ComboBox();
             this.chkReal = new System.Windows.Forms.CheckBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.cbxLoaiHD = new System.Windows.Forms.ComboBox();
             this.grBoxKytra = new System.Windows.Forms.GroupBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cbxKytra = new System.Windows.Forms.ComboBox();
+            this.cbxDonVi = new System.Windows.Forms.ComboBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.optTraNL = new System.Windows.Forms.RadioButton();
             this.optTraDK = new System.Windows.Forms.RadioButton();
             this.label10 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.cbxDonVi = new System.Windows.Forms.ComboBox();
-            this.label11 = new System.Windows.Forms.Label();
-            this.cbxKytra = new System.Windows.Forms.ComboBox();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.txtDate = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
             this.cmdAccept = new System.Windows.Forms.Button();
             this.cmdClose = new System.Windows.Forms.Button();
+            this.cbxDonvitinh = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxDateContracts = new System.Windows.Forms.DateTimePicker();
             this.grBoxKytra.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -78,23 +81,23 @@ namespace Quanlydongtien
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 86);
+            this.label4.Location = new System.Drawing.Point(12, 89);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(40, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "So tien";
             // 
-            // textBox1
+            // txtTongtien
             // 
-            this.textBox1.Location = new System.Drawing.Point(107, 86);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(162, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtTongtien.Location = new System.Drawing.Point(107, 86);
+            this.txtTongtien.Name = "txtTongtien";
+            this.txtTongtien.Size = new System.Drawing.Size(121, 20);
+            this.txtTongtien.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(284, 91);
+            this.label5.Location = new System.Drawing.Point(235, 89);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(30, 13);
             this.label5.TabIndex = 8;
@@ -109,30 +112,30 @@ namespace Quanlydongtien
             this.label6.TabIndex = 10;
             this.label6.Text = "Ky han";
             // 
-            // comboBox4
+            // cbxKyhan
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(107, 124);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(99, 21);
-            this.comboBox4.TabIndex = 9;
+            this.cbxKyhan.FormattingEnabled = true;
+            this.cbxKyhan.Location = new System.Drawing.Point(107, 124);
+            this.cbxKyhan.Name = "cbxKyhan";
+            this.cbxKyhan.Size = new System.Drawing.Size(60, 21);
+            this.cbxKyhan.TabIndex = 9;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(212, 127);
+            this.label7.Location = new System.Drawing.Point(235, 127);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(44, 13);
             this.label7.TabIndex = 12;
             this.label7.Text = "Lai suat";
             // 
-            // comboBox5
+            // cbxLaisuat
             // 
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Location = new System.Drawing.Point(270, 124);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(62, 21);
-            this.comboBox5.TabIndex = 11;
+            this.cbxLaisuat.FormattingEnabled = true;
+            this.cbxLaisuat.Location = new System.Drawing.Point(285, 124);
+            this.cbxLaisuat.Name = "cbxLaisuat";
+            this.cbxLaisuat.Size = new System.Drawing.Size(62, 21);
+            this.cbxLaisuat.TabIndex = 11;
             // 
             // chkReal
             // 
@@ -147,11 +150,11 @@ namespace Quanlydongtien
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(339, 128);
+            this.label8.Location = new System.Drawing.Point(354, 128);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.Size = new System.Drawing.Size(37, 13);
             this.label8.TabIndex = 14;
-            this.label8.Text = "Nam";
+            this.label8.Text = "%Nam";
             // 
             // label9
             // 
@@ -193,62 +196,30 @@ namespace Quanlydongtien
             this.grBoxKytra.TabStop = false;
             this.grBoxKytra.Text = "Tra no";
             // 
-            // radioButton1
+            // txtDate
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(230, 19);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(87, 17);
-            this.radioButton1.TabIndex = 2;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Tra nhieu lan";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.txtDate.Location = new System.Drawing.Point(230, 50);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(96, 20);
+            this.txtDate.TabIndex = 25;
             // 
-            // optTraNL
+            // label13
             // 
-            this.optTraNL.AutoSize = true;
-            this.optTraNL.Location = new System.Drawing.Point(113, 19);
-            this.optTraNL.Name = "optTraNL";
-            this.optTraNL.Size = new System.Drawing.Size(87, 17);
-            this.optTraNL.TabIndex = 1;
-            this.optTraNL.TabStop = true;
-            this.optTraNL.Text = "Tra nhieu lan";
-            this.optTraNL.UseVisualStyleBackColor = true;
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(168, 50);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(47, 13);
+            this.label13.TabIndex = 24;
+            this.label13.Text = "Ngay tra";
             // 
-            // optTraDK
+            // label12
             // 
-            this.optTraDK.AutoSize = true;
-            this.optTraDK.Location = new System.Drawing.Point(16, 19);
-            this.optTraDK.Name = "optTraDK";
-            this.optTraDK.Size = new System.Drawing.Size(78, 17);
-            this.optTraDK.TabIndex = 0;
-            this.optTraDK.TabStop = true;
-            this.optTraDK.Text = "Tra dinh ky";
-            this.optTraDK.UseVisualStyleBackColor = true;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(12, 170);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(34, 13);
-            this.label10.TabIndex = 18;
-            this.label10.Text = "Mo ta";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(107, 167);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(271, 20);
-            this.textBox2.TabIndex = 19;
-            // 
-            // cbxDonVi
-            // 
-            this.cbxDonVi.FormattingEnabled = true;
-            this.cbxDonVi.Location = new System.Drawing.Point(91, 47);
-            this.cbxDonVi.Name = "cbxDonVi";
-            this.cbxDonVi.Size = new System.Drawing.Size(61, 21);
-            this.cbxDonVi.TabIndex = 20;
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(29, 50);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(58, 13);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "Don vi tinh";
             // 
             // label11
             // 
@@ -267,30 +238,65 @@ namespace Quanlydongtien
             this.cbxKytra.Size = new System.Drawing.Size(61, 21);
             this.cbxKytra.TabIndex = 21;
             // 
-            // label12
+            // cbxDonVi
             // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(29, 50);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(58, 13);
-            this.label12.TabIndex = 23;
-            this.label12.Text = "Don vi tinh";
+            this.cbxDonVi.FormattingEnabled = true;
+            this.cbxDonVi.Location = new System.Drawing.Point(91, 47);
+            this.cbxDonVi.Name = "cbxDonVi";
+            this.cbxDonVi.Size = new System.Drawing.Size(61, 21);
+            this.cbxDonVi.TabIndex = 20;
             // 
-            // label13
+            // radioButton1
             // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(168, 50);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(47, 13);
-            this.label13.TabIndex = 24;
-            this.label13.Text = "Ngay tra";
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(230, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(87, 17);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Tra nhieu lan";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
             // 
-            // txtDate
+            // optTraNL
             // 
-            this.txtDate.Location = new System.Drawing.Point(230, 50);
-            this.txtDate.Name = "txtDate";
-            this.txtDate.Size = new System.Drawing.Size(96, 20);
-            this.txtDate.TabIndex = 25;
+            this.optTraNL.AutoSize = true;
+            this.optTraNL.Location = new System.Drawing.Point(113, 19);
+            this.optTraNL.Name = "optTraNL";
+            this.optTraNL.Size = new System.Drawing.Size(87, 17);
+            this.optTraNL.TabIndex = 1;
+            this.optTraNL.TabStop = true;
+            this.optTraNL.Text = "Tra nhieu lan";
+            this.optTraNL.UseVisualStyleBackColor = true;
+            this.optTraNL.CheckedChanged += new System.EventHandler(this.optTraNL_CheckedChanged);
+            // 
+            // optTraDK
+            // 
+            this.optTraDK.AutoSize = true;
+            this.optTraDK.Location = new System.Drawing.Point(16, 19);
+            this.optTraDK.Name = "optTraDK";
+            this.optTraDK.Size = new System.Drawing.Size(78, 17);
+            this.optTraDK.TabIndex = 0;
+            this.optTraDK.TabStop = true;
+            this.optTraDK.Text = "Tra dinh ky";
+            this.optTraDK.UseVisualStyleBackColor = true;
+            this.optTraDK.CheckedChanged += new System.EventHandler(this.optTraDK_CheckedChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 170);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Mo ta";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(107, 167);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(271, 20);
+            this.txtDesc.TabIndex = 19;
             // 
             // cmdAccept
             // 
@@ -312,14 +318,47 @@ namespace Quanlydongtien
             this.cmdClose.UseVisualStyleBackColor = true;
             this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
             // 
+            // cbxDonvitinh
+            // 
+            this.cbxDonvitinh.FormattingEnabled = true;
+            this.cbxDonvitinh.Items.AddRange(new object[] {
+            "Ngay",
+            "Thang",
+            "Nam"});
+            this.cbxDonvitinh.Location = new System.Drawing.Point(173, 124);
+            this.cbxDonvitinh.Name = "cbxDonvitinh";
+            this.cbxDonvitinh.Size = new System.Drawing.Size(55, 21);
+            this.cbxDonvitinh.TabIndex = 22;
+            this.cbxDonvitinh.Text = "Ngay";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(282, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Ngay hop dong";
+            // 
+            // cbxDateContracts
+            // 
+            this.cbxDateContracts.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.cbxDateContracts.Location = new System.Drawing.Point(285, 85);
+            this.cbxDateContracts.Name = "cbxDateContracts";
+            this.cbxDateContracts.Size = new System.Drawing.Size(98, 20);
+            this.cbxDateContracts.TabIndex = 24;
+            // 
             // NhapthongtinHD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(390, 364);
+            this.Controls.Add(this.cbxDateContracts);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbxDonvitinh);
             this.Controls.Add(this.cmdClose);
             this.Controls.Add(this.cmdAccept);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtDesc);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.grBoxKytra);
             this.Controls.Add(this.label9);
@@ -327,14 +366,18 @@ namespace Quanlydongtien
             this.Controls.Add(this.label8);
             this.Controls.Add(this.chkReal);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.cbxLaisuat);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.cbxKyhan);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtTongtien);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.cbxMaKH);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(398, 398);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(398, 398);
             this.Name = "NhapthongtinHD";
             this.Text = "NhapthongtinHD";
             this.Load += new System.EventHandler(this.NhapthongtinHD_Load);
@@ -350,12 +393,12 @@ namespace Quanlydongtien
         private System.Windows.Forms.ComboBox cbxMaKH;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtTongtien;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox cbxKyhan;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox cbxLaisuat;
         private System.Windows.Forms.CheckBox chkReal;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
@@ -365,7 +408,7 @@ namespace Quanlydongtien
         private System.Windows.Forms.RadioButton optTraNL;
         private System.Windows.Forms.RadioButton optTraDK;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtDesc;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.ComboBox cbxKytra;
         private System.Windows.Forms.ComboBox cbxDonVi;
@@ -374,5 +417,8 @@ namespace Quanlydongtien
         private System.Windows.Forms.TextBox txtDate;
         private System.Windows.Forms.Button cmdAccept;
         private System.Windows.Forms.Button cmdClose;
+        private System.Windows.Forms.ComboBox cbxDonvitinh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker cbxDateContracts;
     }
 }
--- Source/Quanlydongtien/Quanlydongtien/NhapthongtinHD.cs
+++ Source/Quanlydongtien/Quanlydongtien/NhapthongtinHD.cs
@@ -12,8 +12,13 @@ namespace Quanlydongtien
 {
     public partial class NhapthongtinHD : Form
     {
+        const int TraDinhKy = 1;
+        const int TraNhieuLan = 2;
+        const int TraMotLan = 3;
         db contractDb;
-
+        Boolean edit;
+        string MaHD;
+        int HTTra;
         public NhapthongtinHD()
         {
             InitializeComponent();
@@ -21,7 +26,28 @@ namespace Quanlydongtien
 
         private void cmdAccept_Click(object sender, EventArgs e)
         {
-
+            string sqlStr;
+            string tongtien;
+            if (cbxLoaiHD.Text == "Cho vay")
+                tongtien = "-" + txtTongtien.Text;
+            else tongtien = txtTongtien.Text;
+            if (edit == true)
+            {
+                sqlStr = "UPDATE [HOPDONG] SET [Real] = " + chkReal.Enabled.ToString();
+                sqlStr = sqlStr + ", [DESC] = '" + txtDesc.Text + "' WHERE [MaHD] = " + MaHD;
+                contractDb.runSQLCmd(sqlStr);
+            }
+            else
+            {
+                sqlStr = "INSERT INTO [HOPDONG] ([MaKH], [NgayHD], [Tongtien], [Real], [Kyhan], [DonVT], [Laisuat], [Desc]";
+                sqlStr = sqlStr + ", [Hoanthanh], [NoQH], [Tratruoc], [Hinhthuctra]) VALUES ('";
+                sqlStr = sqlStr + cbxMaKH.Text + "', '" + cbxDateContracts.Value.ToShortDateString();
+                sqlStr = sqlStr + "', " + tongtien + ", " + !chkReal.Checked.ToString();
+                sqlStr = sqlStr + ", " + cbxKyhan.Text + ", '" + cbxDonvitinh.Text;
+                sqlStr = sqlStr + "', " + cbxLaisuat.Text + ", '" + txtDesc.Text;
+                sqlStr = sqlStr + "', No, No, No, " + HTTra + ")";
+                contractDb.runSQLCmd(sqlStr);
+            }
         }
 
         private void cmdClose_Click(object sender, EventArgs e)
@@ -35,17 +61,73 @@ namespace Quanlydongtien
             string sqlStr;
             OleDbDataReader oleReader;
             contractDb = new db(dbname);
+            edit = false;
+            MaHD = "";
+            HTTra = TraDinhKy;
+            optTraDK.Checked = true;
             if (contractDb == null)
             {
                 MessageBox.Show("Loi ket noi den database");
                 this.Close();
             }
-            sqlStr = "SELECE [MaKH] FROM [KHACHHANG]";
+            sqlStr = "SELECT [MaKH] FROM [KHACHHANG] ORDER BY [MaKH]";
             try
             {
                 oleReader = contractDb.genDataReader(sqlStr);
                 while (oleReader.Read())
-                    
+                {
+                    cbxMaKH.Items.Add(oleReader[0].ToString());
+                }
+                cbxMaKH.Text = cbxMaKH.Items[0].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                contractDb.close();
+                this.Close();
+            }
+        }
+        public void init(string dbname, string maHD)
+        {
+            string sqlStr;
+            OleDbDataReader oleReader;
+            contractDb = new db(dbname);
+            Int64 tongtien = 0;
+            MaHD = maHD;
+            edit = true;
+            if (contractDb == null)
+            {
+                MessageBox.Show("Loi ket noi den database");
+                this.Close();
+            }
+
+            sqlStr = "SELECE [MaKH], [NgayHD], [Tongtien], [Real], [Kyhan], [DonVT], [Laisuat], [Desc] FROM [HOPDONG]";
+            sqlStr = sqlStr + "[MaHD] = " + maHD;
+            try
+            {
+                oleReader = contractDb.genDataReader(sqlStr);
+                while (oleReader.Read())
+                {
+                    cbxMaKH.Text = oleReader["MaKH"].ToString();
+                    cbxMaKH.Enabled = false;
+                    tongtien = Int64.Parse(oleReader["Tongtien"].ToString());                    
+                    cbxKyhan.Text = oleReader["Kyhan"].ToString();
+                    cbxKyhan.Enabled = false;
+                    cbxDonvitinh.Text = oleReader["DonVT"].ToString();
+                    cbxDonvitinh.Enabled = false;
+                    chkReal.Checked = Boolean.Parse(oleReader["Real"].ToString());
+                    cbxDateContracts.Value = DateTime.ParseExact(oleReader["NgayHD"].ToString(), "dd-MM-yyyy", null);
+                    cbxDateContracts.Enabled = false;
+                    txtDesc.Text = oleReader["Desc"].ToString();
+                    cbxLaisuat.Text = oleReader["Laisuat"].ToString();
+                    cbxLaisuat.Enabled = false;
+                }
+                if (tongtien > 0)
+                    cbxLoaiHD.Text = cbxLoaiHD.Items[0].ToString();
+                else cbxLoaiHD.Text = cbxLoaiHD.Items[1].ToString();
+                cbxLoaiHD.Enabled = false;
+                cbxMaKH.Text = cbxMaKH.Items[0].ToString();
+                grBoxKytra.Enabled = false;
             }
             catch (Exception ex)
             {
@@ -63,6 +145,22 @@ namespace Quanlydongtien
         private void NhapthongtinHD_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void optTraDK_CheckedChanged(object sender, EventArgs e)
+        {
+            HTTra = TraDinhKy;
+            cbxKytra.Enabled = true;
+        }
+
+        private void optTraNL_CheckedChanged(object sender, EventArgs e)
+        {
+            HTTra = TraNhieuLan;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            HTTra = TraMotLan;
         }
     }
 }
